a3fc552a146ebce03151740dce34eacc756ad1ab
Aaron Tam <aarontam9529@gmail.com>
2024-02-01 22:26:38 +1000

    Updated: contact info

--- src/Content.jsx
+++ src/Content.jsx
@@ -82,17 +82,22 @@ export default function Content() {
             <span>Projects</span>
           </Col>
         </Row>
-        <Row className="row-cols-1 mt-4 main-project">
+        <Row className="row-cols-1 main-project">
           <Col className="card-col">
-            <Card bg="dark" text="white" align="left" className="mt-3 mb-5 z-0">
-              <Card.Header className="ms-4 z-0">Advice Slip</Card.Header>
+            <Card
+              bg="dark"
+              text="white"
+              align="left"
+              className="mt-3 z-0 border-0"
+            >
               <Card.Body>
+                <p className="ms-4 mb-4">Advice Slip</p>
                 <Card.Img
-                  className="p-4 z-0"
+                  className="px-4"
                   variant="top"
                   src="advice_slip.PNG"
                 />
-                <Card.Text className="px-4 z-0">
+                <Card.Text className="pt-4 px-4">
                   Generate random advice slips or search advice slips with
                   keywords or ID.
                 </Card.Text>
@@ -137,13 +142,16 @@ export default function Content() {
             </Card>
           </Col>
           <Col>
-            <Card bg="dark" text="white" align="left" className="mt-3 mb-5">
-              <Card.Header className="ms-4">
-                Fire Evacuation Simulation
-              </Card.Header>
+            <Card
+              bg="dark"
+              text="white"
+              align="left"
+              className="mt-3 z-0 border-0"
+            >
               <Card.Body>
-                <Card.Img className="p-4" variant="top" src="fire_evac.PNG" />
-                <Card.Text className="px-4">
+                <p className="ms-4">Fire Evacuation Simulation</p>
+                <Card.Img className="px-4" variant="top" src="fire_evac.PNG" />
+                <Card.Text className="pt-4 px-4">
                   Simulation of a fire evacuation situation in a stadium for
                   event organizers.
                 </Card.Text>
@@ -175,11 +183,16 @@ export default function Content() {
             </Card>
           </Col>
           <Col>
-            <Card bg="dark" text="white" align="left" className="mt-3 mb-5">
-              <Card.Header className="ms-4">Tech</Card.Header>
+            <Card
+              bg="dark"
+              text="white"
+              align="left"
+              className="mt-3 z-0 border-0"
+            >
               <Card.Body>
-                <Card.Img className="p-4" variant="top" src="tech.PNG" />
-                <Card.Text className="px-4">
+                <p className="ms-4">Tech</p>
+                <Card.Img className="px-4" variant="top" src="tech.PNG" />
+                <Card.Text className="pt-4 px-4">
                   A made-up Technology Company's website showcasing their range
                   of product.
                 </Card.Text>
@@ -217,6 +230,32 @@ export default function Content() {
                 </ButtonGroup>
               </Card.Body>
             </Card>
+          </Col>
+        </Row>
+        <Row className="row-cols-1 main-contact" align="left">
+          <Col className="mb-4">
+            <span>Contacts</span>
+          </Col>
+          <Col>
+            <p>
+              Email <img src="email.PNG" width={20} /> aarontam9529@gmail.com
+            </p>
+          </Col>
+          <Col>
+            <p>
+              GitHub <img src="github.PNG" width={20} />{" "}
+              <a href="https://github.com/tlkaaa" target="_blank">
+                github.com/tlkaaa ↗
+              </a>
+            </p>
+          </Col>
+          <Col>
+            <p>
+              LinkedIn <img src="linkedin.PNG" width={20} />{" "}
+              <a href="https://www.linkedin.com/in/aarontamlk/" target="_blank">
+                www.linkedin.com/in/aarontamlk ↗
+              </a>
+            </p>
           </Col>
         </Row>
       </Container>
